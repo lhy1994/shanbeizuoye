20ba0d1270e00e5629c045aa8fb8df3bbe5f8d6d
lhy <844873090@qq.com>
2016-06-01 20:01:01 +0800

    改进生词本界面

--- app/src/main/java/com/liuhaoyuan/myreader/WordActivity.java
+++ app/src/main/java/com/liuhaoyuan/myreader/WordActivity.java
@@ -1,86 +1,107 @@
 package com.liuhaoyuan.myreader;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.liuhaoyuan.myreader.R;
+import com.huxq17.swipecardsview.BaseCardAdapter;
+import com.huxq17.swipecardsview.SwipeCardsView;
 import com.liuhaoyuan.myreader.utils.DatabaseUtils;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class WordActivity extends AppCompatActivity {
 
-    private ListView listView;
     private ArrayList<String> wordList;
-    private WordListAdapter adapter;
+    private MyCardAdapter cardAdapter;
+    private SwipeCardsView swipeCardsView;
+    private Button look;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word);
-        listView = (ListView) findViewById(R.id.list_word);
+        swipeCardsView = (SwipeCardsView) findViewById(R.id.swipCardsView);
+        look = (Button) findViewById(R.id.btn_look);
         initData();
     }
-    public void initData(){
-        wordList= DatabaseUtils.queryWords(this);
-        adapter = new WordListAdapter();
-        listView.setAdapter(adapter);
+
+    public void initData() {
+        wordList = DatabaseUtils.queryWords(this);
+//        adapter = new WordListAdapter();
+//        listView.setAdapter(adapter);
+        cardAdapter = new MyCardAdapter(wordList, this);
+        swipeCardsView.setAdapter(cardAdapter);
+        look.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swipeCardsView.setAdapter(cardAdapter);
+                swipeCardsView.notifyDatasetChanged(0);
+            }
+        });
     }
 
-    class WordListAdapter extends BaseAdapter{
+    public class MyCardAdapter extends BaseCardAdapter {
+        private List<String> datas;
+        private Context context;
+
+        public MyCardAdapter(List<String> datas, Context context) {
+            this.datas = datas;
+            this.context = context;
+
+        }
 
         @Override
         public int getCount() {
-            return wordList.size();
+            return datas.size();
         }
 
         @Override
-        public Object getItem(int position) {
-            return null;
+        public int getCardLayoutId() {
+            return R.layout.word_list_item;
         }
 
         @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            final ViewHolder holder;
-            if (convertView==null){
-                convertView=View.inflate(WordActivity.this,R.layout.word_list_item,null);
-                holder=new ViewHolder();
-                holder.word= (TextView) convertView.findViewById(R.id.tv_wordnote);
-                holder.delete= (ImageButton) convertView.findViewById(R.id.btn_delete);
-                convertView.setTag(holder);
-            }else {
-                holder= (ViewHolder) convertView.getTag();
+        public void onBindData(final int position, View cardview) {
+            if (datas == null||datas.size()==0) {
+                return;
             }
-            holder.word.setText(wordList.get(position));
-            holder.delete.setOnClickListener(new View.OnClickListener() {
+
+            final TextView textView = (TextView) cardview.findViewById(R.id.tv_wordnote);
+            final ImageButton button = (ImageButton) cardview.findViewById(R.id.btn_delete);
+
+//            if (position==datas.size()-1){
+//                datas.add("没有生词了，快去添加！");
+//                button.setVisibility(View.GONE);
+//            }
+            textView.setText(datas.get(position));
+            button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    DatabaseUtils.deleteWord(WordActivity.this,holder.word.getText().toString());
-                    Toast.makeText(WordActivity.this,"删除成功",Toast.LENGTH_SHORT).show();
-                    wordList=DatabaseUtils.queryWords(WordActivity.this);
-                    adapter.notifyDataSetChanged();
+                    DatabaseUtils.deleteWord(WordActivity.this, textView.getText().toString());
+                    Toast.makeText(WordActivity.this, "删除成功", Toast.LENGTH_SHORT).show();
+                    datas.remove(position);
+                    button.setVisibility(View.GONE);
+                    swipeCardsView.setAdapter(cardAdapter);
+                    swipeCardsView.notifyDatasetChanged(0);
                 }
             });
-            return convertView;
         }
-    }
-    static class ViewHolder{
-        TextView word;
-        ImageButton delete;
+
+        /**
+         * 如果可见的卡片数是3，则可以不用实现这个方法
+         *
+         * @return
+         */
+        @Override
+        public int getVisibleCardCount() {
+            return datas.size();
+        }
     }
 }
